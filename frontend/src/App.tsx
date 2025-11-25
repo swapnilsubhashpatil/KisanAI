b6bdbfeb4e789a335eb7865231410c9534fdf331
swapnilsubhashpatil <patilswapnilsubhash@gmail.com>
2025-11-25 10:52:15 +0530

    Add routing configuration

---
+++ frontend/src/App.tsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Navbar } from "./components/Navbar"
+import { Home } from "./pages/Home"
+import Vision from "./pages/Vision"
+import Monitoring from "./pages/Monitoring"
+import { MarketInsights } from "./pages/MarketInsights"
+import Water from "./pages/SmartFarming"
+import Chatbot from "./components/Chatbot"
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+        <Chatbot />
+        <main className="pt-16">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/monitor"
+              element={<Monitoring />}
+            />
+            <Route path="/consult" element={<Vision />} />
+            <Route path="/market" element={<MarketInsights />} />
+            <Route path="/farming" element={<Water />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  )
+}
+
+export default App
